af0d71f032d307ad42eb5d2f0fce6deed15052e4
laxmi <laxmi@laxmiputra>
2022-01-23 12:27:42 +0530

    added test4 and test5

---
+++ src/com/arrays/Test4.java
@@ -0,0 +1,12 @@
+package com.arrays;
+
+public class Test4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello123");
+
+	}
+
+}
---
+++ src/com/arrays/Test5.java
@@ -0,0 +1,12 @@
+package com.arrays;
+
+public class Test5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello123");
+
+	}
+
+}
